d9a0361a043c980f459753e41061958a3900e036
citeFred <citefred@gmail.com>
2025-10-10 16:07:52 +0900

    chore: First Init

---
+++ src/main/java/com/toonsquare/esg_showcase_app/EsgShowcaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.toonsquare.esg_showcase_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgShowcaseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsgShowcaseAppApplication.class, args);
+    }
+
+}
